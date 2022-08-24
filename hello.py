04a17146252b7cab7f25e0473187b5740cb23f83
Ronald-Muller <57322215+Ronald-Muller@users.noreply.github.com>
2022-08-24 17:07:25 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+x=int(input(''))
+z=str(input(''))
+y=int(input(''))
+a=int(x+y)
+b=int(x-y)
+c=int(x*y)
+d=int(x/y)
+if z=='+':
+    print(a)
+if z=='-':
+    print(b)
+if z=='*':
+    print(c)
+if z=='/':
+    print(d)
